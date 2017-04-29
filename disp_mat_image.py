64438aca00e2a57af38c1a461e1b6c8c01fd562a
Akrit2013 <yan2006nt@sina.com>
2017-04-29 21:06:26 +0800

    update disp mat image

--- disp_mat_image.py
+++ disp_mat_image.py
@@ -4,6 +4,8 @@
 # in the mat file
 
 import getopt
+import image_tools
+import log_tools
 import sys
 import imshow_lib
 import matlab_tools
@@ -13,12 +15,15 @@ def main(argv):
     in_file = None
     var_name = None
 
+    is_transpose = False
+
     help_msg = 'disp_mat_image.py -i <mat> -v [var]\n\
 -i <mat>        The input mat file\n\
--v [var]        The var name in the mat'
+-v [var]        The var name in the mat\n\
+-t              Transpose the image before display'
 
     try:
-        opts, args = getopt.getopt(argv, 'hi:v:')
+        opts, args = getopt.getopt(argv, 'hi:v:t')
     except getopt.GetoptError:
         print help_msg
         sys.exit(2)
@@ -31,15 +36,26 @@ def main(argv):
             in_file = arg
         elif opt == '-v':
             var_name = arg
+        elif opt == '-t':
+            is_transpose = True
 
     if in_file is None:
         print help_msg
         sys.exit()
 
     im = matlab_tools.load_mat(in_file, var_name)
+    im = image_tools.preprocess(im)
+    if is_transpose:
+        if len(im.shape) == 2:
+            im = im.transpose()
+        elif len(im.shape) == 3:
+            im = im.transpose([1, 0, 2])
+        else:
+            log_tools.log_err('Can not display the image with \
+shape %s' % str(im.shape))
+
     ish = imshow_lib.Imshow()
     ish.autoshow(im)
-
     raw_input('Press Enter to exist ...')
 
 
